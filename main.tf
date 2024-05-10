08b5d46caab335f0c228c529c3e267a867802b71
philipphomberger <philipp.homberger@gmail.com>
2024-05-10 21:29:08 +0200

    add some stuff

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   cloud {
-#     organization = "homberger"
+terraform {
+  cloud {
+    organization = "homberger"
 
-#     workspaces {
-#       name = "learningaws"
-#     }
-#   }
-# }
+    workspaces {
+      name = "learningaws"
+    }
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
@@ -25,11 +25,7 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--region", "us-east-1", "--cluster-name", var.cluster_name]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
